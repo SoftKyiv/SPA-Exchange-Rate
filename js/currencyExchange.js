0d6a5614086aee798aa47805e03f7071721b6d99
Vladislav Lapidus <45798764+ITLapidus@users.noreply.github.com>
2022-02-06 18:17:26 +0300

    Add Currence Page, Home Page, Assets, SPA

--- js/currencyExchange.js
+++ js/currencyExchange.js
@@ -1,8 +1,10 @@
-const input = document.querySelector('#input');
-const result = document.querySelector('#result');
-const select = document.querySelector('#select');
+const input = document.querySelector("#input");
+const result = document.querySelector("#result");
+const select = document.querySelector("#select");
 input.oninput = convertValueCur;
 select.oninput = convertValueCur;
 function convertValueCur() {
-   result.value = (parseFloat(input.value) / ratesCur[select.value].Value).toFixed(2);
+  result.value = (
+    parseFloat(input.value) / ratesCur[select.value].Value
+  ).toFixed(2);
 }
